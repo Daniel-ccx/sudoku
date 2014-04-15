2804f3a38a8cd0072305f980b49df0d855b58203
Daniel <faithsmine@gmail.com>
2014-04-15 15:07:21 +0800

    有的数独题目解不开，解题算法有问题

--- php/crack.php
+++ php/crack.php
@@ -28,73 +28,67 @@ class crack {
             }
             else
             {
-                if($x < 8)
-                {
-                    $y = 0;
-                    if($this->solve($x+1, $y))
-                        return true;
-                    return false;
-
-                }
+                $y = 0;
+                if($this->solve($x+1, $y))
+                    return true;
+                $this->array_sudoku[$x][$y] = '';
             }
         }
-
-        for($n = 1; $n < 10; $n++)
+        else
         {
-            if(in_array($n, $this->array_sudoku[$x]))
-                continue;
+            for($n = 1; $n < 10; $n++)
+            {
+                if(in_array($n, $this->array_sudoku[$x]))
+                    continue;
 
-            $flag = true;
-            //检测列
-            for($j = 0; $j < 9; $j++)
-            {
-                if($this->array_sudoku[$j][$y] == $n)
+                $flag = true;
+                //检测列
+                for($j = 0; $j < 9; $j++)
                 {
-                    $flag = false;
-                    break;
-                }
-            }
-            if(!$flag)
-                continue;
-            //检测九宫格
-            $d_x = floor($x / 3) * 3;
-            $d_y = floor($y / 3) * 3;
-            for($j = 0; $j < 3; $j++)
-            {
-                for($k = 0; $k < 3; $k++)
-                {
-                    if($this->array_sudoku[$d_x + $j][$d_y + $k] == $n)
+                    if($this->array_sudoku[$j][$y] == $n)
                     {
-                        $flag =false;
+                        $flag = false;
                         break;
                     }
                 }
                 if(!$flag)
-                    break;
-            }
+                    continue;
+                //检测九宫格
+                $d_x = floor($x / 3) * 3;
+                $d_y = floor($y / 3) * 3;
+                for($j = 0; $j < 3; $j++)
+                {
+                    for($k = 0; $k < 3; $k++)
+                    {
+                        if($this->array_sudoku[$d_x + $j][$d_y + $k] == $n)
+                        {
+                            $flag =false;
+                            break;
+                        }
+                    }
+                    if(!$flag)
+                        break;
+                }
+                if(!$flag)
+                    continue;
 
-            //开始设置
-            $this->array_sudoku[$x][$y] = $n;
-            if($y < 8)
-            {
-                if($this->solve($x, $y + 1))
-                    return true;
-            }
-            else
-            {
-
-                if($x < 8)
+                //开始设置
+                $this->array_sudoku[$x][$y] = $n;
+                if($y < 8)
+                {
+                    if($this->solve($x, $y + 1))
+                        return true;
+                }
+                else
                 {
                     $y = 0;
                     if($this->solve($x + 1, $y))
                         return true;
                 }
-                else
-                    return true;
+
+                $this->array_sudoku[$x][$y] = '';
+
             }
-
-            $this->array_sudoku[$x][$y] = '';
-
         }
         return false;
     }
--- php/index.php
+++ php/index.php
@@ -6,17 +6,7 @@ include_once(_ROOT_ . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'sudok
 $sudoku = new sudoku();
 $original = $sudoku->generate();
 
-
 $sudoku_arr = $sudoku->getSudoku();
-foreach($sudoku_arr as $k => $v)
-{
-    echo $k . ' =>>>> ';
-    foreach($v as $j => $i)
-    {
-        echo '['.$j.']=>('.$i.')';
-    }
-    echo "<hr>";
-}
 
 //获取破解
 include_once(_ROOT_ . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'crack.php');
@@ -25,8 +15,22 @@ $crack = new crack();
 $crack->setParams($sudoku_arr);
 $crack->solve();
 $solution = $crack->getSolution();
+echo "<style>table td{width: 30px; height:30px; border: 1px solid green;text-align:center;}</style>";
+
+echo "item:`";
+echo "<table>";
+foreach($sudoku_arr as $k => $v)
+{
+    echo "<tr>";
+    foreach($v as $j => $i)
+    {
+        echo "<td name='g_{$k}{$j}'>{$i}</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
+
 echo "original:<br>";
-echo "<style>table td{width: 30px; height:30px;}</style>";
 echo "<table>";
 foreach($original as $k => $v)
 {
